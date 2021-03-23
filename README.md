ae7c66a38065109e12ebde59ed1223fb455f879a
Scott Strattner <sstrattn@us.ibm.com>
2021-03-23 16:09:38 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# OCP Source-to-image (S2I) example using Python Flask web app framework
-This github will provide a basic pyflask app that can be used to showcase the OCP S2I capability.
-Using github webhooks, one can also showcase Continuous Deployment (CD) capability by automating application deployment
+# Get network address
+A very simple Flask app that, given a host IP and prefix, returns the network address and subnet mask.
+One step above "Hello, World", the purpose is to demonstrate the Source to Image (S2I) functionality of
+OpenShift.
+
+## Example
+To test this, run directly with *python app.py* and access via curl:
+```
+$ curl http://127.0.0.1:8080/192.168.45.10/17
+192.168.0.0 255.255.128.0
+```
+## Inspiration
+More than just inspired by, this was started by a git clone of
+https://github.com/ocp-power-demos/s2i-pyflask-demo
+But since the app.py was changed completely, it didn't seem right
+to treat it just as a fork.
--- app.py
+++ app.py
@@ -1,17 +1,31 @@
+"""
+Given a host IP and prefix (mask bits), return
+the network address and subnet mask. Example:
+/192.168.1.34/24
+192.168.1.0 255.255.255.0
+"""
+import inspect
+import ipaddress
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/<host_ip>/<prefix>')
+def netaddr(host_ip, prefix):
+    """Return network address and subnet mask."""
+    ip_address = host_ip + '/' + prefix
+    try:
+        ip_net = ipaddress.ip_network(ip_address, False)
+        return str(ip_net.network_address) + ' ' + str(ip_net.netmask)
+    except ValueError:
+        return 'Invalid IP/prefix format'
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World ! - Pyflask Demo'
-
-@app.route('/version')
-def get_version():
-    return 'App version : <b>1.0</b>'
-
-@app.route('/test')
-def get_test():
-    return 'You are accessing /test endpoint'
+def directions():
+    """Provide help."""
+    help_screen = inspect.cleandoc("""
+        Call this service with URL containing: /<ip_address>/<prefix>
+        Example: /192.168.1.34/24""")
+    return help_screen
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
